da5f731bfefd7b5d5ea1d8e72d697f50c823fb78
Felix Chan <felixchan29@gmail.com>
2020-08-18 12:02:50 -0400

    sample code

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "semantic-ui-react"
+
+const Title = (props) => {
+  return(
+    <Container>
+        <h1>Title</h1>
+    </Container>
+  );
+};
+
+export default Title;
