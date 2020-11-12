29f4f8be182ff9769ec2bf4a7cd3b19bf69e1742
Felix Chan <felixchan29@gmail.com>
2020-11-12 11:12:26 -0500

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react'
+
+class NavBar extends Component {
+  handleItemClick = (e, { name }) => this.setState({  })
+
+  render() {
+    const { activeItem } = this.props
+
+    return (
+      <Menu floated="right">
+        <NavLink to="/">
+          <Menu.Item name='home' active={activeItem === 'score'} onClick={this.handleItemClick} />
+        </NavLink>
+        <NavLink to="/budgeting-tips">
+          <Menu.Item name='budgeting' active={activeItem === 'score2'} onClick={this.handleItemClick} />
+        </NavLink>
+        <NavLink to="/debt-free-living">
+            <Menu.Item name='debt-free_living' active={activeItem === 'user'} onClick={this.handleItemClick} />
+        </NavLink>
+        <NavLink to="/make-money">
+            <Menu.Item name='make_money' active={activeItem === 'game'} onClick={this.handleItemClick} />
+        </NavLink>
+        <NavLink to="/save-money">
+            <Menu.Item name='save_money' active={activeItem === 'game2'} onClick={this.handleItemClick} />
+        </NavLink>
+        <NavLink to="/debt-snowball-calculator">
+            <Menu.Item name='debt_snowball_calculator' active={activeItem === 'secret'} onClick={this.handleItemClick} />
+        </NavLink>
+        <NavLink to="/resources">
+            <Menu.Item name='resources' active={activeItem === 'logout'} onClick={this.handleLogout} />
+        </NavLink>
+      </Menu>
+    )
+  }
+}
+
+
+export default NavBar
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Container } from "semantic-ui-react"
-
+// Home Budgeting Debt-Free Living Make Money Save MoneyDebt Snowball CalculatorAbout ContactDisclaimerPrivacy PolicyTerms & Conditions
 const Title = (props) => {
   return(
     <Container>
-        <h1>Title</h1>
+        <h1>Beat The Budget</h1>
     </Container>
   );
 };
